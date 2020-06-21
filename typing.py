48d4bb852db832882798b7cedaa387e12be83454
SyfSchydea <sashapwn@gmail.com>
2020-06-21 11:12:42 +0100

    Also track stats for failed attempts

--- typing.py
+++ typing.py
@@ -70,7 +70,12 @@ reset_weight = CSI + "22m"
 #
 # max_fails - How many failed attempts before a failure. Pass negative to not fail.
 #
-# returns (bool success, float time_taken)
+# returns (
+# 	bool  success     - Did the user successfully complete the line?
+# 	float time_taken  - How long did they take on the successful attempt. None if they failed.
+# 	int   total_chars - How many characters did the user type in total, including failed attempts?
+# 	int   total_time  - How long did the user spend on all attempts? Excludes time spent between attempts.
+# )
 def practice_line(string, max_fails=10):
 	# Print the string for the user to copy.
 	sys.stdout.write(fg(CYAN) + bold + string + reset + "\n")
@@ -78,6 +83,10 @@ def practice_line(string, max_fails=10):
 	str_progress = 0
 	start_time = None
 	failures_remaining = int(max_fails)
+
+	total_chars = 0
+	total_time = 0
+
 	while str_progress < len(string):
 		# Read a character of user input
 		user_char = get_char()
@@ -88,6 +97,8 @@ def practice_line(string, max_fails=10):
 		if user_char == correct_char:
 			sys.stdout.write(correct_char)
 			sys.stdout.flush()
+
+			total_chars += 1
 
 			if str_progress == 0:
 				start_time = time.time()
@@ -108,11 +119,14 @@ def practice_line(string, max_fails=10):
 			sys.stdout.write(fg(RED) + bold + display_char + reset + "\n")
 			sys.stdout.flush()
 
+			total_time += time.time() - start_time
+			total_chars += 1
+
 			start_time = None
 
 			failures_remaining -= 1
 			if failures_remaining == 0:
-				return False, None
+				return False, None, total_chars, total_time
 
 			str_progress = 0
 
@@ -122,11 +136,17 @@ def practice_line(string, max_fails=10):
 	if start_time is not None:
 		end_time = time.time()
 		time_elapsed = end_time - start_time
+		total_time += time_elapsed
 
-	return True, time_elapsed
+	return True, time_elapsed, total_chars, total_time
 
 # Practice a passage consisting of several lines
 def practice_passage(lines, fail_lines=10):
+	# Time/chars on successful attempts
+	total_correct_time = 0
+	total_correct_chars = 0
+
+	# Time/chars on all attempts
 	total_time = 0
 	total_chars = 0
 	
@@ -134,18 +154,23 @@ def practice_passage(lines, fail_lines=10):
 	while current_progress < len(lines):
 		line = lines[current_progress]
 
-		success, time_taken = practice_line(line, max_fails=fail_lines)
+		success, time_taken, total_line_chars, total_line_time = practice_line(line, max_fails=fail_lines)
+
+		total_time += total_line_time
+		total_chars += total_line_chars
 
 		if success:
-			total_time += time_taken
-			total_chars += len(line)
+			total_correct_time += time_taken
+			total_correct_chars += len(line)
 			current_progress += 1
 		elif current_progress > 0:
 			current_progress -= 1
 	
-	wpm = total_chars / total_time * 12
+	wpm_correct = total_correct_chars / total_correct_time * 12
+	wpm_all     = total_chars         / total_time         * 12
 	
-	sys.stdout.write(fg(GREEN) + f"Speed: {wpm:.1f} wpm" + reset + "\n")
+	sys.stdout.write(fg(GREEN) + f"Speed (all): {    wpm_all    :.1f} wpm" + reset + "\n")
+	sys.stdout.write(fg(GREEN) + f"Speed (correct): {wpm_correct:.1f} wpm" + reset + "\n")
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser("Typing Practice")
